4483bc63278a66780e724da7a41216d26c95301b
suns1191 <sagarneeli1191@gmail.com>
2015-05-06 03:56:18 -0400

    Linear Regression

---
+++ Practise Challenges/linear_regression.py
@@ -0,0 +1,39 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+
+loansData = pd.read_csv('https://spark-public.s3.amazonaws.com/dataanalysis/loansData.csv')
+cleanInterestRate = loansData['Interest.Rate'].map(lambda x: round(float(x.rstrip('%')) / 100, 4))
+loansData['Interest.Rate'] = cleanInterestRate
+
+cleanLoanData = loansData['Loan.Length'].map(lambda x: int(x.rstrip(' months')))
+loansData['Loan.Length'] = cleanLoanData
+
+cleanFICORange = loansData['FICO.Range'].map(lambda x: x.split('-'))
+cleanFICORange = cleanFICORange.map(lambda x: [int(n) for n in x])
+loansData['FICO.Range'] = cleanFICORange
+loansData['FICO.Score'] = [cleanFICORange.values[x][0] for x in range(len(cleanFICORange))]
+
+# plt.figure()
+# p = loansData['FICO.Score'].hist()
+# a = pd.scatter_matrix(loansData, alpha=0.05, figsize=(10,10), diagonal='hist')
+# plt.show()
+
+intrate = loansData['Interest.Rate']
+loanamt = loansData['Amount.Requested']
+fico = loansData['FICO.Score']
+
+# The dependent variable
+y = np.matrix(intrate).transpose()
+# The independent variables shaped as columns
+x1 = np.matrix(fico).transpose()
+x2 = np.matrix(loanamt).transpose()
+# put the two columns together to create an input matrix
+x = np.column_stack([x1,x2])
+X = sm.add_constant(x)
+model = sm.OLS(y,X)
+f = model.fit()
+
+f.summary()
